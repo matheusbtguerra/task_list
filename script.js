9536913095d8eba3fafb233c98e6b5b828c914ae
matheusbtguerra <146946503+matheusbtguerra@users.noreply.github.com>
2024-03-26 09:27:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,25 @@ function removeTask(button) {
     ul.removeChild(li);
 }
 
-const chk = document.getElementById('chk');
-chk.style.backgroundColor = "#fff";
+const chk = document.getElementById('chk')
+const themeSystem = localStorage.getItem("themeSystem") || "light"
 
-chk.addEventListener('change', () => {
-    document.body.style.backgroundColor = "#000"});
+chk.addEventListener('click', () => {
+    let oldTheme = localStorage.getItem("themeSystem") || "light";
+    let newTheme = oldTheme == "light" ? "dark" : "light";
+
+    localStorage.setItem("themeSystem", newTheme)
+    defineCurrentTheme(newTheme)
+})
+
+function defineCurrentTheme(theme){
+    document.documentElement.setAttribute("data-theme", theme)
+
+    if(theme == "light") {
+       const switchbg = document.getElementById("swt").style.backgroundColor = "#E7DFD8";
+    } else {
+        const switchbg = document.getElementById("swt").style.backgroundColor = "#574533";
+    }
+}
+
+defineCurrentTheme(themeSystem)
